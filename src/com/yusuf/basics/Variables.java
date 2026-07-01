07e6385545a8ebe52c5194326b78d44e9aa00167
yusuf-in <mohammadyusuf.work@gmail.com>
2026-07-01 17:44:37 +0000

    learn types of variables

---
+++ src/com/yusuf/basics/Variables.java
@@ -0,0 +1,17 @@
+package com.yusuf.basics;
+
+public class Variables {
+    public static void main(String[] args) {
+
+        int age = 20;   // Integer Variable
+        double cgpa = 7.19;  // Double Variable
+        char grade = 'A';   // Characters -> A, B, C .....
+        boolean placed = false;  // True or False
+        String name = "Yusuf";  // String
+
+        System.out.println(age);
+        System.out.println(name);
+        System.out.println(grade);
+        System.out.println(cgpa);
+    }
+}
