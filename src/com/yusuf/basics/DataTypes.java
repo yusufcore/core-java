31f9c105ce189b50f49c120f30baef41b9849932
yusuf-in <mohammadyusuf.work@gmail.com>
2026-07-02 04:50:35 +0000

    learn Data Types in Java

---
+++ src/com/yusuf/basics/DataTypes.java
@@ -0,0 +1,49 @@
+package com.yusuf.basics;
+
+public class DataTypes {
+    public static void main(String[] args) {
+
+        byte age = 20;
+        short year = 2026;
+        int salary = 700000;
+        long population = 1430000000L;
+
+        float pi = 3.14f;
+        double cgpa = 7.19;
+
+        char grade = 'A';
+
+        boolean isPlaced = false;
+
+        boolean backend = true;
+        char letter = 'Y';
+        int CTC = 1200000;
+
+        String name = "Mohammad Yusuf";
+
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Year: " + year);
+        System.out.println("Salary: " + salary);
+        System.out.println("Population: " + population);
+        System.out.println("PI: " + pi);
+        System.out.println("CGPA: " + cgpa);
+        System.out.println("Grade: " + grade);
+        System.out.println("Placed: " + isPlaced);
+        System.out.println("\n");
+        System.out.println("Student Details \n----------------");
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("CGPA: " + cgpa);
+        System.out.println("Java Backend: " + backend);
+        System.out.println("Favourite Letter: " +letter );
+        System.out.println("Expected Package: " + CTC);
+
+
+
+
+
+
+
+    }
+}
