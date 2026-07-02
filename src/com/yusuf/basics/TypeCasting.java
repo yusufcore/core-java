84798a643e5e3324109081bacc56f068f1d15aa8
yusuf-in <mohammadyusuf.work@gmail.com>
2026-07-02 05:05:04 +0000

    learn Type Casting in Java

---
+++ src/com/yusuf/basics/TypeCasting.java
@@ -0,0 +1,23 @@
+package com.yusuf.basics;
+
+public class TypeCasting {
+    public static void main(String[] args) {
+
+        int num = 10;
+        double price = 99.999;
+        char letter = 'a';
+
+        double value = num;   // Implicit Casting ---> Automatic;
+        System.out.println(value);
+
+        int amount = (int) price;   // Explicit Casting ----> Manual;
+        System.out.println(amount);
+
+//        int ascii = letter;    // Implicit Way
+        int ascii = (int) letter;  // Explicit way
+        System.out.println(ascii);
+
+
+
+    }
+}
