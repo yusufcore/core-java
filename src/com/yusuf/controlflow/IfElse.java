c112e414339c7ba7cab09c2ed135282edc7560aa
yusuf-in <mohammadyusuf.work@gmail.com>
2026-07-02 14:15:43 +0000

    Add If Else statements in Java

---
+++ src/com/yusuf/controlflow/IfElse.java
@@ -0,0 +1,14 @@
+package com.yusuf.controlflow;
+
+public class IfElse {
+    public static void main(String[] args) {
+
+        int age = 17;
+
+        if(age>=18){
+            System.out.println("You are an adult.");
+        }else{
+            System.out.println("You are a minor.");
+        }
+    }
+}
